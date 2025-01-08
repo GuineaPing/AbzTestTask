8628a03a108468f07af800b24ae44a5398114b42
GuineaPing <guineaping@gmail.com>
2025-01-08 14:04:32 +0200

    Initial Commit

---
+++ AbzTestTask/AbzTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbzTestTaskApp.swift
+//  AbzTestTask
+//
+//  Created by Eugene Lysenko on 08.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AbzTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
